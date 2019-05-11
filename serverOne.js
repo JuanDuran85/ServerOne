6e5063c3bb2bb2f4134d1bf5205c2868efe49383
JuanDuran85 <duran85juan@gmail.com>
2019-05-11 16:21:14 -0400

    Mostrando respuesta en la pagina del navegador web

--- serverOne.js
+++ serverOne.js
@@ -1,6 +1,8 @@
 let http = require('http');
 
-let server = http.createServer(function () {
+let server = http.createServer( function (require,resolve) {
+    resolve.writeHead(200,{'Content-type':'text/html'}); //escribe en el encabezado del documento cuando la respuesta es 200
+    resolve.write(`Respuesta correcta para la direccion: ${require.url}`); //escribe la cadena de texto dentro de la respuesta del servidor
     console.log("Inicio de Servidor");
 });
 
